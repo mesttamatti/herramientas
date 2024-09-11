2cbef448600ff26ae3cd379cb9917c39dda1ce3b
Peluca2094 <mati_todoeso@hotmail.com>
2024-09-11 09:21:20 -0300

    Update herramientas.h

--- herramientas.h
+++ herramientas.h
@@ -18,9 +18,10 @@ using namespace std;
 
 
 //////detallo la clase de herramientas///////////
-class herramientas {
+class herramientas
+{
 
-private:
+protected:
     string _nombre;
     float _peso;
     float  _longitud;
@@ -28,14 +29,12 @@ private:
 ///constructor/////////
 
 public:
-    herramientas (string nombre,float peso_1,float longitud_1);
+    herramientas (float peso_1,float longitud_1);
 
-  float get_peso();
-  float get_longitud();
-   string get_nombre();
-  void set_peso(float peso_1);
-  void set_longitud(float longitud_1);
-
+    float get_peso();
+    float get_longitud();
+    void set_peso(float peso_1);
+    void set_longitud(float longitud_1);
 
 
 
