1351632efdaf31a96455d5f9b2fbdb308abbb0cc
mesttamatti <105522615+mesttamatti@users.noreply.github.com>
2024-09-12 19:36:55 -0300

    Update herramientas.h
    code review con arreglos. Cosas minimas, lo mas importante era que faltaba encerrar en llaves el constructor

--- herramientas.h
+++ herramientas.h
@@ -17,8 +17,8 @@ using namespace std;
 
 
 
-//////detallo la clase de herramientas///////////
-class herramientas
+//////detallo la clase de herramientas/////////// Madu: Las clases van con Mayuscula al principio
+class Herramientas
 {
 
 protected:
@@ -26,16 +26,16 @@ protected:
     float _peso;
     float  _longitud;
 
-///constructor/////////
+///constructor/////////Madu: faltaba encerrarlo en llaves
 
 public:
-    herramientas (float peso_1,float longitud_1);
+    Herramientas (float peso_1,float longitud_1){
 
     float get_peso();
     float get_longitud();
     void set_peso(float peso_1);
     void set_longitud(float longitud_1);
-
+    };
 
 
 
